efc8a613731998e7f7e9201f6d68bbaf4fc0b882
Ben Shakow <benshakow@gmail.com>
2019-02-22 09:58:52 -0500

    Made sentiment testing into it's own class

---
+++ STest.py
@@ -0,0 +1,41 @@
+#Ben Shakow
+#A class based version of the sentiment test
+rom __future__ import absolute_import, division, print_function
+from keras.models import load_model
+import tensorflow as tf
+import keras
+from keras.preprocessing.text import Tokenizer
+import numpy as np
+import pickle
+
+
+class STest:
+    model = load_model('modelSave2.h5')
+
+    with open('tokenizer.pickle', 'rb') as handle:
+        token = pickle.load(handle)
+
+    def test(self):
+        print(hello)
+
+
+    def testPhrase(self, phrase):
+        doc = keras.preprocessing.text.text_to_word_sequence(phrase, filters='!"#%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=" ")
+        for l in range(len(doc)):
+            temp = doc[l]
+            doc[l] = token.word_index.get(temp)
+            if (doc[l] == None):
+                doc[l]=0
+        while len(doc)<35:
+            doc.append(0)
+        data = []
+        data.append(doc)
+        data = np.array(data)
+    #    print(data)
+        tested = model.predict(data)
+        print('The phrase "'+ phrase + '" is ', end='')
+        if(tested[0]<=.5):
+            print('bad :(')
+        else:
+            print('good!')
+        print('\n')
